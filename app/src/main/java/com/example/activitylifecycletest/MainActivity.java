4a87e434cb429e63619954efa74d4885651ba50e
xupengfei <15303933327@163.com>
2019-01-24 21:30:04 +0800

    init

---
+++ app/src/main/java/com/example/activitylifecycletest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.activitylifecycletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startNormalActivity = (Button)findViewById(R.id.start_normal_activity);
+    }
+}
